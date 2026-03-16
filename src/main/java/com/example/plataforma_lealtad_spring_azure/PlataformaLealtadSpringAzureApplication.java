2260ae6f181451d7ceec2fa7604c0e1acde15597
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-03-16 12:07:15 -0600

    initial commit

---
+++ src/main/java/com/example/plataforma_lealtad_spring_azure/PlataformaLealtadSpringAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.plataforma_lealtad_spring_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaLealtadSpringAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaLealtadSpringAzureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/plataforma_lealtad_spring_azure/PlataformaLealtadSpringAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plataforma_lealtad_spring_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaLealtadSpringAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
